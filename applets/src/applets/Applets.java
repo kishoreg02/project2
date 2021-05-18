c7a3ba28974d70de22bc78736523fa347f300566
ELCOT <ELCOT@Lenovo>
2021-05-19 00:58:10 +0530

    first commit uploading applet files

---
+++ applets/src/applets/Applets.java
@@ -0,0 +1,12 @@
+package applets;
+
+import java.applet.*;
+import java.awt.*;
+
+
+public class Applets extends Applet {
+	
+	   public void paint (Graphics g) {
+	      g.drawString ("Hello World", 25, 50);
+	   }
+}
